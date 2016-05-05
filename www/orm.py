db70f2db47454964a48781bae4f9b7b1c3910758
bbos1994 <bbos1994@163.com>
2016-05-05 23:42:20 +0800

    修改手滑

--- www/orm.py
+++ www/orm.py
@@ -14,7 +14,7 @@ def log(sql,args=()):
 def create_pool(loop,**kw):
 	logging.info('create database connection pool...')
 	global __pool
-	_pool=yield from aiomysql.create_pool(
+	__pool=yield from aiomysql.create_pool(
 		host = kw.get('host','localhost'),
 		port = kw.get('port',3306),
 		user = kw['user'],

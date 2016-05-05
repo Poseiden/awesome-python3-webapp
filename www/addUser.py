6824ee1f4f971e5448945763dee40a115d7f25ac
bbos1994 <bbos1994@163.com>
2016-05-05 23:23:53 +0800

    修正一些手误

--- www/addUser.py
+++ www/addUser.py
@@ -4,6 +4,7 @@
 import orm,asyncio
 from models import User,Blog,Comment
 
+@asyncio.coroutine
 def test(loop):
 	yield from orm.create_pool(loop = loop,user='www-data',password='www-data',db='awesome')
 	u = User(name = 'Test',email = 'test@example.com',passwd = '1234567890',image = 'about:blank')

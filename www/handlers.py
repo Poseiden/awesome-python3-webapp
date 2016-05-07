748c2fcfb924fe3520510630a6d6aa6c7b678ad2
bbos1994 <bbos1994@163.com>
2016-05-07 11:14:03 +0800

    day 07

--- www/handlers.py
+++ www/handlers.py
@@ -4,3 +4,18 @@
 __author__='TonyZhu'
 
 ' url handlers '
+
+import re,time,json,logging,hashlib,base64,asyncio
+
+from coroweb import get,post
+
+from models import User,Comment,Blog,next_id
+
+@get('/')
+@asyncio.coroutine
+def index(request):
+	users = yield from User.findAll()
+	return {
+		'__template__':'test.html'
+		'users':users
+	}
